6e2c0ee10765357ed095a4add4a7023ec2eba305
rockybalboa2831 <rockybalboa2831@hotmail.com>
2025-09-18 11:33:47 -0400

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,16 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(policy =>
+    policy.AllowAnyHeader()
+          .AllowAnyMethod()
+          .WithOrigins("https://localhost:4200", "http://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
